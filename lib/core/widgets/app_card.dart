d235f37fd8115d325be9ba034c950f9d8bf57703
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:48:24 +0300

    Update and rename app_text_field.dart to app_card.dart

---
+++ lib/core/widgets/app_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry padding;
+  final VoidCallback? onTap;
+
+  const AppCard({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(18),
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: const Color(0xff1B1F2A),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(22),
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(22),
+        onTap: onTap,
+        child: Padding(
+          padding: padding,
+          child: child,
+        ),
+      ),
+    );
+  }
+}

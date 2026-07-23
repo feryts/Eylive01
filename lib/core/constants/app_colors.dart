85ffe17e24f1822f715a1a3c362cc86abbd9eba1
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:35:25 +0300

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF6C4DFF);
+  static const Color secondary = Color(0xFFFF5CA8);
+
+  static const Color background = Color(0xFF0F1117);
+
+  static const Color card = Color(0xFF1B1F2A);
+
+  static const Color white = Colors.white;
+
+  static const Color grey = Color(0xFF9CA3AF);
+
+  static const Color border = Color(0xFF2B3245);
+
+  static const Color textField = Color(0xFF181C27);
+}

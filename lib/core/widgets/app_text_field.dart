d52a370401b3fbc12db324c0a9d903a48ec2be88
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:36:22 +0300

    Create app_text_field.dart

---
+++ lib/core/widgets/app_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_colors.dart';
+
+class AppTextField extends StatelessWidget {
+  final String hint;
+
+  final bool obscure;
+
+  final TextEditingController controller;
+
+  const AppTextField({
+    super.key,
+    required this.hint,
+    required this.controller,
+    this.obscure = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscure,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: const TextStyle(color: Colors.grey),
+        filled: true,
+        fillColor: AppColors.textField,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}

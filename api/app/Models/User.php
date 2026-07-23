b97b2fef32a2b95a503c6aad4283acbfce8aafcf
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:04:05 +0300

    Update User.php

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,31 +2,72 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    protected $fillable = [
+
+        'ey_id',
+
+        'first_name',
+        'last_name',
+        'username',
+
+        'email',
+        'phone',
+
+        'password',
+
+        'google_id',
+        'apple_id',
+
+        'avatar',
+
+        'birth_date',
+
+        'gender',
+
+        'country',
+
+        'coins',
+        'diamonds',
+
+        'vip_level',
+        'vip_expired_at',
+
+        'is_broadcaster',
+        'agency_id',
+
+        'trust_score',
+
+        'is_banned',
+
+        'last_login_ip',
+        'last_login_at',
+
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
+            'vip_expired_at' => 'datetime',
+            'last_login_at' => 'datetime',
+
             'password' => 'hashed',
+
+            'is_broadcaster' => 'boolean',
+            'is_banned' => 'boolean',
         ];
     }
 }

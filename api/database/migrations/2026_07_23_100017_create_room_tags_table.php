64bb15705427ace200acf2cd2c0fd530989b1ac4
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 13:27:58 +0300

    Create 2026_07_23_100017_create_room_tags_table.php

---
+++ api/database/migrations/2026_07_23_100017_create_room_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('room_tags', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('name',50);
+
+            $table->string('icon')->nullable();
+
+            $table->boolean('is_active')
+                ->default(true);
+
+            $table->timestamps();
+
+            $table->unique('name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('room_tags');
+    }
+};

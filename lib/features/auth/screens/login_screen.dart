35282f432828657722a67670ec9b8dc8c6814ea7
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:34:52 +0300

    Create login_screen.dart

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "EyLive",
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}

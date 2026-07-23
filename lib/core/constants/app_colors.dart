f489ffe11d38f66e22fc29220a3d63865a720c3c
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:49:58 +0300

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  AppColors._();
+  static const Color primary = Color(0xff6C4DFF);
 
-  static const Color primary = Color(0xFF6C4DFF);
-  static const Color secondary = Color(0xFFFF5CA8);
+  static const Color secondary = Color(0xffFF5CA8);
 
-  static const Color background = Color(0xFF0F1117);
+  static const Color background = Color(0xff0F1117);
 
-  static const Color card = Color(0xFF1B1F2A);
+  static const Color card = Color(0xff1B1F2A);
 
-  static const Color white = Colors.white;
+  static const Color success = Color(0xff4CAF50);
 
-  static const Color grey = Color(0xFF9CA3AF);
+  static const Color warning = Color(0xffFFC107);
 
-  static const Color border = Color(0xFF2B3245);
+  static const Color danger = Color(0xffF44336);
 
-  static const Color textField = Color(0xFF181C27);
+  static const Color textPrimary = Colors.white;
+
+  static const Color textSecondary = Color(0xffB0B3C7);
 }

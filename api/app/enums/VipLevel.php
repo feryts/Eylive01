3775570e49e26719fbb364f90faf13fd3c46914d
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:09:17 +0300

    Create VipLevel.php

---
+++ api/app/enums/VipLevel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Enums;
+
+enum VipLevel:int
+{
+
+    case NONE=0;
+
+    case VIP1=1;
+
+    case VIP2=2;
+
+    case VIP3=3;
+
+    case VIP4=4;
+
+    case VIP5=5;
+
+    case VIP6=6;
+
+    case VIP7=7;
+
+}

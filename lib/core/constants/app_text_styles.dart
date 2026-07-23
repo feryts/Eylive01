67caf6e76365e432ac72b6cd98c13962a0bf7e7b
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:50:21 +0300

    Create app_text_styles.dart

---
+++ lib/core/constants/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle title = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle heading = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    color: Colors.white,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 13,
+    color: Color(0xffB0B3C7),
+  );
+
+  static const TextStyle button = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+}

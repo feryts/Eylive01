7c07dc0bc7bc418a6bc3deda30a5aa0f1746abab
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:07:32 +0300

    Change root route to return JSON response

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return response()->json([
+        'application' => 'EyLive API',
+        'version' => '1.0.0',
+        'status' => 'online',
+    ]);
+
 });

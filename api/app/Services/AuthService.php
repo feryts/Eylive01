bc632ae9f0e8caa3c8ab4c7b734ea924d84e0f3c
feryts <81550388+feryts@users.noreply.github.com>
2026-07-23 12:18:31 +0300

    Create AuthService.php

---
+++ api/app/Services/AuthService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+class AuthService
+{
+    public function register(array $data)
+    {
+        // Kayıt işlemleri burada olacak.
+    }
+
+    public function login(array $data)
+    {
+        // Giriş işlemleri burada olacak.
+    }
+
+    public function logout()
+    {
+        // Çıkış işlemleri burada olacak.
+    }
+}
